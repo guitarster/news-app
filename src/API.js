0406cbb5285a3f71359cca342fcd4ffc3d03e5d1
guitarster <guitarster@posteo.de>
2025-11-19 16:00:47 +0100

    new design

--- src/API.js
+++ src/API.js
@@ -1,10 +1,5 @@
-// const API_BASE_URL = "https://gnews.io/api/v4/";
-// const API_KEY = "c010ad7b97c2e9b11d64507305952d9e";
-
 export async function getTopNews(feedUrl) {
-  const url = feedUrl;
-
-  const response = await fetch(url);
+  const response = await fetch(feedUrl);
 
   const responseJSON = await response.json();
 
--- src/startscreen.js
+++ src/startscreen.js
@@ -20,7 +20,7 @@ export async function renderStartscreen() {
   renderHeader(appStart);
   renderCategories(appStart);
   renderNews(appStart);
-  loadNews("https://rss.app/feeds/v1.1/8pn1E3Yx2PUufYkS.json");
+  await loadNews("https://rss.app/feeds/v1.1/8pn1E3Yx2PUufYkS.json");
 
   const schlagzeilenEl = document.getElementById("schlagzeilen");
   schlagzeilenEl.addEventListener("click", () =>
@@ -46,6 +46,26 @@ export async function renderStartscreen() {
   sportEl.addEventListener("click", () =>
     loadNews("https://rss.app/feeds/v1.1/tv9nw7FZOVEFCRqy.json")
   );
+
+  const bookmarkEl = document.getElementsByClassName(
+    "card__date-bookmark__bookmark"
+  );
+  for (let element of bookmarkEl) {
+    element.addEventListener("click", () => saveBookmark(element));
+  }
+}
+
+function saveBookmark(element) {
+  element.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 256 256">
+            <g fill="#1da9af">
+                <g transform="scale(10.66667,10.66667)">
+                <path d="M19,20.6l-7,-2.6l-7,2.6v-16.6c0,-0.5 0.4,-1 1,-1h12c0.6,0 1,0.4 1,1z"></path>
+                <path d="M4,22v-18c0,-1.1 0.9,-2 2,-2h12c1.1,0 2,0.9 2,2v18l-8,-3zM12,16.9l6,2.3v-15.2h-12v15.1z"></path>
+                </g>
+            </g>
+        </svg>
+`;
 }
 
 function renderApp() {
@@ -56,11 +76,11 @@ function renderApp() {
 function renderHeader(appStart) {
   appStart.innerHTML += `
     <div class="header">
+        <div class="header__title">Newsly</div>
         <div class="header__search-button"><svg fill="#f1f1f2" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
             <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
             </svg>
         </div>
-        <div class="header__title">Newsly</div>
     </div>`;
 }
 
@@ -90,7 +110,7 @@ function renderCard(news, loadedNews) {
   const durationSincePublishing = currentTime - dateStampParsed;
 
   news.innerHTML += `
-    <div class="card">
+    <div class="card" data-id="${loadedNews.id}">
         <a href="${loadedNews.url}" target="blank">
             <div class="card__wrapper-for-link">
                 <div class="card__image"><img width="320" src="${
@@ -105,8 +125,8 @@ function renderCard(news, loadedNews) {
                 <div class="card__date-bookmark__date">Vor ${Math.round(
                   (durationSincePublishing / (1000 * 60 * 60)) % 24
                 )} h veröffentlicht</div>
-                <div class="card__date-bookmark__bookmark"><svg fill="#1995ad" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
-                    <path d="M 6.0097656 2 C 4.9143111 2 4.0097656 2.9025988 4.0097656 3.9980469 L 4 22 L 12 19 L 20 22 L 20 20.556641 L 20 4 C 20 2.9069372 19.093063 2 18 2 L 6.0097656 2 z M 6.0097656 4 L 18 4 L 18 19.113281 L 12 16.863281 L 6.0019531 19.113281 L 6.0097656 4 z"></path>
+                <div class="card__date-bookmark__bookmark"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0,0,256,256">
+                    <g fill="#1da9af" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M6.00977,2c-1.09545,0 -2,0.9026 -2,1.99805l-0.00977,18.00195l8,-3l8,3v-1.44336v-16.55664c0,-1.09306 -0.90694,-2 -2,-2zM6.00977,4h11.99023v15.11328l-6,-2.25l-5.99805,2.25z"></path></g></g>
                     </svg>
                 </div>
             </div>
